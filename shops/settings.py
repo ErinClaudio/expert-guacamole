692277badc9fb6b737a86243d2aba01ffd1c561b
Erin Claudio <eclaudio@gmail.com>
2020-03-21 11:50:14 -0400

    grammer error fixed

--- shops/settings.py
+++ shops/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')h
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'         #this was added as part of tutorial
